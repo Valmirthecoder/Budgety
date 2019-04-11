e89ce3fa8b052338f0c704580f94745ae1661097
Valmirthecoder <valmir.haxholli@student.cacttus.education>
2019-04-11 13:41:48 +0200

    Getting input values and modyfing ui controller

--- app.js
+++ app.js
@@ -1,31 +1,60 @@
+
+//Budget Controller
+
 var budgetController = (function(){
 
-    var x = 23;
 
-    var add = function(a){
-      return x+a;
-    }
-    return {
-      publicTest: function(b){
-        return (add(b));
-      }
-    }
 })();
 
-
+//UI CONTROLLER
 var UIController = (function(){
 
-    //code
+    var DOMstrings = {
+      inputType: '.add__type',
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputBtn: '.add__btn'
+    };
+
+
+    return {
+        getInput: function(){
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+          };
+        },
+
+        getDOMstrings: function(){
+          return DOMstrings;
+        }
+    };
 
 })();
 
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
-    return {
-      anotherPublic: function(){
-        console.log(z);
+    var DOM =UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function(){
+
+      var input = UICtrl.getInput();
+      console.log(input);
+
+
+    };
+
+
+
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+
+    document.addEventListener('keypress',function(event){
+
+      if(event.keyCode === 13 || event.which ===13){
+        ctrlAddItem();
       }
-    }
+    });
 
 })(budgetController, UIController);
